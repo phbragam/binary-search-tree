889372dfd3c492910b2098d738051e4486db15ec
Pedro Braga <phbm.braga@gmail.com>
2023-05-26 16:15:53 -0300

    method for node insertion

--- tree-structures/AugmentedBinarySearchTree.js
+++ tree-structures/AugmentedBinarySearchTree.js
@@ -2,6 +2,35 @@ class AugmentedBinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    // Método para inserir um valor na árvore
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+                newNode.parent = node;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+                newNode.parent = node;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
 }
 
 module.exports = AugmentedBinarySearchTree;
